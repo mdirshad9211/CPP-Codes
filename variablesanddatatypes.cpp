9b18a7f9a0111fdaac34fa3d736de15918619b06
MD IRSHAD <irshadidrisi9211@gmail.com>
2024-04-24 19:21:28 +0530

    variables and datatypes in C++

---
+++ variablesanddatatypes.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Variables or  identifiers   (type name = value;)
+    int num1 = 10;  //num1 -: name of variable
+    int num2 = 20;  //num2 -: name of variable
+    int sum = num1 + num2;  //sum variable name  
+
+
+    //Datatypes
+    //int, char, float, double, bool, void etc 
+    int num = 163;
+    char ch = 'a';
+    float fl = 3.14;
+    double dl = 3.44575654335;
+    bool bl = true;
+    cout<<"size of int "<< sizeof(num)<<endl;
+    cout<<"size of char "<< sizeof(ch)<<endl;
+    cout<<"size of  float "<< sizeof(fl)<<endl;
+    cout<<"size of double "<< sizeof(dl)<<endl;
+    cout<<"size of bool "<< sizeof(bl)<<endl;
+
+
+
+}
