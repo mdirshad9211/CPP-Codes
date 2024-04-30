9edc1b42b3219321e9e8d1443801ff106c667d47
MD IRSHAD <irshadidrisi9211@gmail.com>
2024-04-30 19:05:11 +0530

    practiced some more patterns

--- patterns.cpp
+++ patterns.cpp
@@ -60,6 +60,49 @@ int main(){
     }
 
 
+cout<<endl;
+
+    //Pattern 6
+    for(int i = 1; i<=n; i++){
+                    cout<<"*";
+
+        for(int j = 1; j<n-1; j++){
+           if(i ==1 || i== n){
+            cout<<"*";
+           } else{
+            cout<<" ";
+           }
+            
+        }
+        cout<<"*"<<endl;
+    }
+
+
+    //Pattern 11
+    bool val = true;
+    for (int i = 0; i< n; i++)
+    {
+        for(int j = 0; j<i; j++){
+            cout<<val;
+            val = !val;
+        }
+        cout<<endl;
+    }
+
+
+    //Pattern 12
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n-i; j++){
+            cout<<" ";
+        }
+
+        for(int j = 0; j<n; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
 
     
     
