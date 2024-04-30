befd69e1f0a4ccbd16a7e69e70663d955c1a4a0f
MD IRSHAD <irshadidrisi9211@gmail.com>
2024-04-30 19:05:35 +0530

    added one last pattern

--- patterns.cpp
+++ patterns.cpp
@@ -103,6 +103,22 @@ cout<<endl;
     }
 
 
+    //Pattern 13
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n-i; j++){
+            cout<<" ";
+        }
+
+        for(int j = i; j>=1; j--){
+            cout<<j;
+        }
+        for(int j = 2; j<=i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+
 
     
     
